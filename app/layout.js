270407e1d68ba1e96316566e147d5a72208a1c64
AnilNITT <patidaranil0791@gmail.com>
2023-04-19 10:50:40 +0530

    Add To Do list

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import "../styles/app.scss";
+import Header from "./header";
+import { ContextProvider } from "../components/Clients";
+
+export const metadata = {
+  title: "Todo App",
+  description: "This is a Todo App Project made for Next.js series",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+      </head>
+      <body>
+        <ContextProvider>
+          <>
+            <Header />
+            {children}
+          </>
+        </ContextProvider>
+      </body>
+    </html>
+  );
+}
